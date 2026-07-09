7c1b9d8b7bc5fb8359c63e67909e33cf35fb842f
Surendran Sundara Vadivel | emagine <surendran.vadivel@emagine.org.in>
2026-07-09 22:51:24 +0530

    Add variables for region, instance type, ami, environment, project name

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c02fb55956c7d316"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Project name used for tagging and naming resources"
+  type        = string
+  default     = "ecommerce"
+}
